2b41f550cd093160ad501475aacbbc5aaed9420c
kingaboros <71248858+kingaboros@users.noreply.github.com>
2021-03-24 07:58:22 +0200

    Installed font awesome

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import './App.css';
 
 
 
+//Components
+import Weather from './components/Weather';
 
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="">
-        <h1> Weather App </h1>
-        
-      </header>
-      <footer> 
-        <p> This project was created by{ " " }
-          <a href="https://kngboros-dev.com/" target="_blank">Kinga Boros</a> and is { " " }
-          <a href="https://github.com/kingaboros/weather_app_react.git" target="_blank"> open sourced on GitHub </a>
-        </p>
-      </footer>
+        <header className="app__header">
+              
+        </header>
+
+          <Weather /> 
+
+
+        <footer> 
+          <p> This project was created by{ " " }
+            <a href="https://kngboros-dev.com/" target="_blank">Kinga Boros</a> and is { " " }
+            <a href="https://github.com/kingaboros/weather_app_react.git" target="_blank"> open sourced on GitHub </a>
+          </p>
+        </footer>
       </div>
     </div>
   );
