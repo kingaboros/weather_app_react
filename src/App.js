a14a617a06dd6ffef48064d3dbc002470ebff974
kingaboros <71248858+kingaboros@users.noreply.github.com>
2021-03-24 06:54:54 +0200

    Removed unnecessary files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+
+export default function App() {
+  return (
+    <div className="WeatherApp">
+      <header className="">
+        <h1> Weather App </h1>
+        
+      </header>
+    </div>
+  );
+}
+
+
